958c9ce1a8b20578884d1587d454d557e9e67a69
Akanksha Joshi <joshiakanksha481@gmail.com>
2025-06-29 20:55:21 +0530

    Heap Memory Demo

---
+++ src/HeapMemory/HeapMemoryDemo.java
@@ -0,0 +1,15 @@
+package HeapMemory;
+
+import java.util.ArrayList;
+
+public class HeapMemoryDemo {
+//    Dynamic Memory allocation
+    public static void main(String[] args) {
+        int x =10;
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        System.out.println(list);
+    }
+}
